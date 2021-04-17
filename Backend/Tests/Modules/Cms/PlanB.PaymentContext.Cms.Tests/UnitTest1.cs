0f522b90282f8c6289783fdf0e450e651269195a
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-04-17 22:35:13 +0000

    issue(#13): implementing Cms microservice models

---
+++ Backend/Tests/Modules/Cms/PlanB.PaymentContext.Cms.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PlanB.PaymentContext.Cms.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

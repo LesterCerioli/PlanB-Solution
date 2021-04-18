23f663eb242b0147937b71b148d333be2f0c6e4c
Veronica Silva <veronica.silva@lucastecnologiaservices.onmicrosoft.com>
2021-04-18 18:56:08 +0000

    issue(#15): implementing data module

---
+++ Backend/Application/Infrastructure/PlanB.Infrastructure/Data/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using PlanB.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanB.Infrastructure.Data
+{
+    public interface IRepository<T> : IRepositoryWithTypedId<T, long> where T : IEntityWithTypedId<long>
+    {
+    }
+}

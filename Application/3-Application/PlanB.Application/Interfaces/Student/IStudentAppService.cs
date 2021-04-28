002802e589af7ae2ed49a89dccd554a46ba08f27
Veronica Silva <veronica.silva@lucastecnologiaservices.onmicrosoft.com>
2021-04-28 16:46:44 +0000

    feat(#45): implementing Student Service interface

--- Application/3-Application/PlanB.Application/Interfaces/Student/IStudentAppService.cs
+++ Application/3-Application/PlanB.Application/Interfaces/Student/IStudentAppService.cs
@@ -1,7 +1,13 @@
 namespace PlanB.Application.Interfaces.Student
 {
-    public interface IStudentAppService
+    public interface IStudentAppService : IDisposable
     {
+        Task<IEnumerable<StudentViewModel>> GetAll();
+        Task<StudentViewModel> GetById(Guid id);
+
+        Task<ValidationResult> Register(StudentViewModel studentViewModel);
+        Task<ValidationResult> Update(StudentViewModel studentViewModel);
+        Task<ValidationResult> Remove(StudentViewModel studentViewModel);
          
     }
 }

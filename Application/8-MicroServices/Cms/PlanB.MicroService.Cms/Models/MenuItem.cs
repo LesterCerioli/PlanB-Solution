26d671b729c154e0bd55495ea52d6b3570ab40d9
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2021-04-30 01:02:58 +0000

    fet(#1): implementing entities and GeneratirReport package

---
+++ Application/8-MicroServices/Cms/PlanB.MicroService.Cms/Models/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanB.MicroService.Cms.Models
+{
+    public class MenuItem
+    {
+    }
+}
